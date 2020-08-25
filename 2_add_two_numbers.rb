2de530d9ce1cc02b4830cd4cf201a9bf1ac626b3
Mohit Jain <jain.mohit27@gmail.com>
2020-08-25 11:01:29 +0530

    Add Two numbers stored in linked list

---
+++ 2_add_two_numbers.rb
@@ -0,0 +1,47 @@
+# Leet Code Problem: https://leetcode.com/problems/add-two-numbers/
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def add_two_numbers(l1, l2)
+  head = nil
+  previous_node = nil
+  carry = 0
+  while(!l1.nil? || !l2.nil?)
+    first = l1.nil? ? 0 : l1.val
+    second = l2.nil? ? 0 : l2.val
+    sum = first + second + carry
+    value = sum % 10
+    carry = sum / 10
+    node = ListNode.new(value)
+
+    head = node if head.nil?
+    if previous_node.nil?
+      previous_node = node
+    else
+      previous_node.next = node
+      previous_node = node
+    end
+
+    l1 = l1.next if !l1.nil?
+    l2 = l2.next if !l2.nil?
+  end
+
+  if carry > 0
+    node = ListNode.new(carry)
+    previous_node.next = node
+  end
+
+  head
+end
+
+# You can run this on leet code or
+# feel free to create a linkedlist with node's next added properly
+# to run it on your local machine.
