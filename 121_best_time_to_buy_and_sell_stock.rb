850ff6e112c7e984cb5571e4fb0a2425b315667c
Mohit Jain <jain.mohit27@gmail.com>
2020-09-10 05:58:34 +0530

    Best time to buy and Sell stock

---
+++ 121_best_time_to_buy_and_sell_stock.rb
@@ -0,0 +1,19 @@
+# Leetcode Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+  min_price = nil
+  max_profit = 0
+  prices.each_with_index do |price, index|
+    if min_price.nil? || min_price > price
+      min_price = price
+    elsif price - min_price > max_profit
+      max_profit = price - min_price
+    end
+  end
+
+  max_profit
+end
+
+p max_profit([7, 1, 5, 3, 6, 4])
+p max_profit([7, 6, 4, 3, 1])
