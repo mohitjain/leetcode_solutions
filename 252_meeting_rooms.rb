7802dedc90cd86b78f4f783c81a1af94ef02a8b0
Mohit Jain <jain.mohit27@gmail.com>
2020-09-16 06:26:31 +0530

    Meeting Rooms 1 & 2

---
+++ 252_meeting_rooms.rb
@@ -0,0 +1,32 @@
+#Leetcode Problem: https://leetcode.com/problems/meeting-rooms/
+# Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+#
+#     Example 1:
+#
+#     Input: [[0,30],[5,10],[15,20]]
+# Output: false
+#
+# Example 2:
+#
+#     Input: [[7,10],[2,4]]
+# Output: true
+#
+# NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+#-----------------------------------------------------------------------------------------------------------------------
+# @param {Integer[][]} intervals
+# @return {Boolean}
+def can_attend_meetings(intervals)
+  intervals = intervals.sort{|x, y| x.first  <=> y.first }
+  
+  intervals.each_with_index do |_, index|
+    return true if index == intervals.length - 1
+    if intervals[index][1] > intervals[index + 1][0]
+      return false
+    end
+
+  end
+  true
+end
+
+p can_attend_meetings([[0,30],[5,10],[15,20]])
+p can_attend_meetings [[7,10],[2,4]]
---
+++ 253_meeting_rooms_2.rb
@@ -0,0 +1,42 @@
+# Leetcode Problem: https://leetcode.com/problems/meeting-rooms-ii/
+# Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+#
+#     Example 1:
+#
+#     Input: [[0, 30],[5, 10],[15, 20]]
+# Output: 2
+#
+# Example 2:
+#
+#     Input: [[7,10],[2,4]]
+# Output: 1
+#
+# NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+#-----------------------------------------------------------------------------------------------------------------------
+
+# @param {Integer[][]} intervals
+# @return {Integer}
+def min_meeting_rooms(intervals)
+  start_times = intervals.collect(&:first).sort
+  end_times = intervals.collect(&:last).sort
+  start_point = 0
+  end_point = 0
+  used_rooms = 0
+
+  while start_point < intervals.length
+    if start_times[start_point] >= end_times[end_point]
+      used_rooms = used_rooms - 1
+      end_point = end_point + 1
+    end
+
+    used_rooms = used_rooms + 1
+    start_point = start_point + 1
+
+  end
+
+  used_rooms
+
+end
+
+p min_meeting_rooms([[0, 30],[5, 10],[15, 20]])
+p min_meeting_rooms([[7,10],[2,4]])
