650322de94d17a7cb3af59e0382a072b4e6b231c
Mohit Jain <jain.mohit27@gmail.com>
2020-10-05 15:49:57 +0530

    Minor Fix

--- 14_longest_common_prefix.rb
+++ 14_longest_common_prefix.rb
@@ -25,14 +25,15 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
-  return '' if strs.length == 0
+  return '' if strs.empty?
+
   prefix = ''
   s = strs[0]
   s.length.times.each do |index|
     char = s[index]
     all_has = true
     strs.each do |str|
-      all_has = all_has && str.start_with?(prefix + char)
+      all_has &&= str.start_with?(prefix + char)
     end
     return prefix unless all_has
 
@@ -44,4 +45,4 @@ end
 
 p longest_common_prefix %w[flower flow flight]
 p longest_common_prefix %w[dog racecar car]
-p longest_common_prefix ["aaa","aa","aaa"]
+p longest_common_prefix %w[aaa aa aaa]
