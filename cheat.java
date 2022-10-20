6bd90cd0cf80e1e594a5b569f90875ecabe3242b
Mohit Jain <mohitjain@pop-os.localdomain>
2022-10-20 10:40:26 +0530

    Java Solutions will be added

---
+++ cheat.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Stack;
+
+// This class is just a random class which act as cheat code for Java.
+// I am more comfortable with Ruby thats why it exists only for Java.
+public class cheat {
+
+    public void array(){
+        int[] myArray = new int[5]; // this defines a constant size array.
+        int i = 0;
+
+
+    }
+
+    public void math(){
+        Math.abs(-1);
+    }
+
+    public void stack(){
+        Stack<Integer> stack = new Stack<>();
+
+    }
+
+    public void hashmap(){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for ( Integer key : map.keySet() ) {
+
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet() ) {
+            entry.getKey();
+            entry.getValue();
+        }
+    }
+
+    public void set(){
+        HashSet<Integer> set = new HashSet<>();
+    }
+
+    public void loop(){
+
+    }
+}
